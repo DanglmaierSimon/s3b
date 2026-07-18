fe8820d545133edf09490ce159aa7906270d5b99
Simon Danglmaier <simon@danglmaier.dev>
2026-07-18 13:52:31 +0200

    new c++ project

---
+++ s3b/main.cpp
@@ -0,0 +1,67 @@
+#define _SILENCE_ALL_CXX20_DEPRECATION_WARNINGS 1
+
+
+#include <iostream>
+
+
+
+#include "bot_examples.h"
+#include "sc2api/sc2_api.h"
+#include "sc2api/sc2_gametypes.h"
+#include "sc2utils/sc2_manage_process.h"
+
+int main(int argc, char* argv[]) {
+    sc2::Coordinator coordinator;
+    if (!coordinator.LoadSettings(argc, argv)) {
+        return 1;
+    }
+
+    coordinator.SetMultithreaded(false);
+    coordinator.SetRealtime(true);
+
+    // Add the custom bot, it will control the players.
+    sc2::TerranBot bot1;
+    coordinator.SetParticipants({
+        CreateParticipant(sc2::Race::Terran, &bot1),
+        CreateComputer(sc2::Race::Random, sc2::Difficulty::VeryEasy, sc2::RandomBuild, "stupid computer"),
+        });
+
+    // Start the game.
+    coordinator.LaunchStarcraft();
+
+    const auto mapdir = "C:\\Program Files (x86)\\StarCraft II\\Maps\\";
+
+    const char* map_pool[] = {// "TorchesAIE_v4.SC2Map"
+        // "PersephoneAIE_v4"
+        "Simple128.SC2Map", "Flat128.SC2Map" };
+
+    auto pool_size = std::size(map_pool);
+
+    int low_dist = 0;
+    int high_dist = pool_size;
+    std::srand((unsigned int)std::time(nullptr));
+    auto selection = low_dist + std::rand() % (high_dist - low_dist);
+
+    auto path = std::string(mapdir);
+    path.append(map_pool[selection]);
+
+    std::printf("Chosen map: %s\n", map_pool[selection]);
+
+    bool do_break = false;
+    while (!do_break) {
+        if (!coordinator.StartGame(path)) {
+            break;
+        }
+        while (coordinator.Update() && !do_break) {
+            if (sc2::PollKeyPress()) {
+                do_break = true;
+            }
+        }
+
+        break;
+    }
+
+    bot1.Control()->DumpProtoUsage();
+
+    return 0;
+}
