995007872f0dff25953de6d1d1f4fa854ba52f5d
Simon Danglmaier <simon@danglmaier.dev>
2026-07-26 16:09:12 +0200

    utils moved to bot class

--- src/bot_kq.hh
+++ src/bot_kq.hh
@@ -141,31 +141,180 @@ namespace sc2 {
 
 		inline bool TryExpand(AbilityID build_ability, UnitTypeID worker_type) {
 			const ObservationInterface* observation = Observation();
-			float minimum_distance = std::numeric_limits<float>::max();
-			Point3D closest_expansion;
+			auto max = std::numeric_limits<float>::max();
+			// closest expansion at index [0], second closest at [1]
+			std::array<Point3D, 3> closest_expansions;
+			std::array<float, 3> closest_distances = { max,max,max };
+
 			for (int i = 0; i < expansions_.size(); i++) {
 				float current_distance = Distance2D(observation->GetStartLocation(), expansions_[i]);
 				if (current_distance < .01f) {
 					continue;
 				}
 
-				if (current_distance < minimum_distance) {
+				if (current_distance < closest_distances[0]) {
 					if (Query()->Placement(build_ability, expansions_[i])) {
-						closest_expansion = expansions_[i];
-						minimum_distance = current_distance;
+
+						// shift array elements
+						closest_expansions[2] = closest_expansions[1];
+						closest_expansions[1] = closest_expansions[0];
+
+						closest_distances[2] = closest_distances[1];
+						closest_distances[1] = closest_distances[0];
+
+						closest_expansions[0] = expansions_[i];
+						closest_distances[0] = current_distance;
+					}
+				}
+				else if (current_distance < closest_distances[1])
+				{
+					if (Query()->Placement(build_ability, expansions_[i])) {
+
+						// shift array elements
+						closest_expansions[2] = closest_expansions[1];
+
+						closest_distances[2] = closest_distances[1];
+
+						closest_expansions[1] = expansions_[i];
+						closest_distances[1] = current_distance;
+					}
+				}
+				else if (current_distance < closest_distances[2])
+				{
+					if (Query()->Placement(build_ability, expansions_[i])) {
+
+						closest_expansions[2] = expansions_[i];
+						closest_distances[2] = current_distance;
 					}
 				}
 			}
 			Point3D staging_location_;
 			// only update staging location up till 3 bases.
-			if (TryBuildStructure(build_ability, worker_type, closest_expansion, true) &&
+			for (int i = 0; i < 3; i++)
+			{
+				auto closest = closest_expansions[i];
+
+				if (TryBuildStructure(build_ability, worker_type, closest, true) &&
 				observation->GetUnits(Unit::Self, sc2::IsTownHall()).size() < 4) {
-				staging_location_ = Point3D(((staging_location_.x + closest_expansion.x) / 2),
-					((staging_location_.y + closest_expansion.y) / 2),
-					((staging_location_.z + closest_expansion.z) / 2));
-				return true;
+					staging_location_ = Point3D(((staging_location_.x + closest.x) / 2),
+						((staging_location_.y + closest.y) / 2),
+						((staging_location_.z + closest.z) / 2));
+					return true;
+				}
 			}
 			return false;
+
+		}
+
+
+		inline bool have_enough_supply(const ObservationInterface* obs, double requested_supply)
+		{
+			auto supply_cap = (double)obs->GetFoodCap();
+			auto supply_used = (double)obs->GetFoodUsed();
+
+			// some units have negative supply cost
+			// because they provide supply (overlord)
+			if (requested_supply <= 0)
+			{
+				return true;
+			}
+
+			// supply block (eg overlord killed)
+			if (supply_used > supply_cap)
+			{
+				return false;
+			}
+
+			auto free_supply = supply_cap - supply_used;
+
+			if (free_supply < requested_supply)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		inline bool train(const ObservationInterface* obs, sc2::ActionInterface* actions, sc2::QueryInterface* queries, UNIT_TYPEID type, int amount = 1, bool allow_queueing = true)
+		{
+			using namespace std;
+			auto p_units = get_production_unit(type);
+
+			(void)allow_queueing; // TODO Implement queueing of units -> is this even required?
+
+			if (p_units.empty() || p_units[0] == UNIT_TYPEID::INVALID)
+			{
+				std::cout << "WARNING: Requested builders for " << UnitTypeToName(UnitTypeID(type)) << "(" << UnitTypeID(type) << ")" << " and received none!" << std::endl;
+				return false;
+			}
+
+			auto builder_filter = [&p_units](const Unit& unit) -> bool {return unit.alliance == Unit::Alliance::Self && unit.build_progress == 1.0 && IsUnits{ p_units }(unit);};
+			auto builders = obs->GetUnits(builder_filter);
+
+			const auto& unit_data = obs->GetUnitTypeData();
+
+			auto left_to_train = amount;
+
+			auto pending_units = get_pending_units(obs, type);
+
+			if (pending_units.size() >= builders.size())
+			{
+				return false;
+			}
+
+			int builders_tried = 0;
+
+			while (left_to_train > 0 && have_enough_supply(obs, unit_data[(uint32_t)type].food_required))
+			{
+				auto builder = GetRandomEntry(builders);
+
+				if (builders_tried >= builders.size())
+				{
+					break;
+				}
+
+				// TODO: Allow queuing of stuff
+				if (builder->orders.empty())
+				{
+					// TODO: Use query interface to ask for abilities of unit
+
+					auto abilities = queries->GetAbilitiesForUnit(builder, true, true);
+
+					auto ability = get_production_ability(type);
+					assert(ability != ABILITY_ID::INVALID);
+					std::cout << "INFO: Sending command " << AbilityTypeToName(AbilityID(ability)) << "; " << AbilityID(ability) << " to unit " << UnitTypeToName(builder->unit_type) << "; " << UnitTypeID(builder->unit_type) << std::endl;
+					actions->UnitCommand(builder, ability);
+					left_to_train = std::max(left_to_train - 1, 0);
+				}
+				else
+				{
+					builders_tried += 1;
+				}
+			}
+
+			return left_to_train == 0;
+		}
+
+
+		inline bool can_afford(const ObservationInterface* obs, UNIT_TYPEID type)
+		{
+			const auto& unit_data = obs->GetUnitTypeData();
+			auto idx = static_cast<size_t>(type);
+			auto mineral_cost = unit_data[idx].mineral_cost;
+			auto gas_cost = unit_data[idx].vespene_cost;
+			double supply_cost = std::ceil(unit_data[idx].food_required);
+
+			if (obs->GetMinerals() >= mineral_cost
+				&& obs->GetVespene() >= gas_cost
+				&& (have_enough_supply(obs, supply_cost)))
+			{
+				return true;
+			}
+			else {
+			return false;
+			}
 		}
 
 
--- src/utils.h
+++ src/utils.h
@@ -7,6 +7,7 @@
 #include "sc2lib/sc2_utils.h"
 
 #include "unit_data.h"
+#include <sc2_unit_filters.h>
 
 namespace sc2 {
 
@@ -21,38 +22,8 @@ namespace sc2 {
 		return get_pending_buildings(obs, [](const Unit&) -> bool {return true;});
 	}
 
-	inline Units get_pending_units(const ObservationInterface* obs, UNIT_TYPEID type)
-	{
-		auto f = [type](const Unit& unit) -> bool {
-			if (unit.unit_type.ToType() == UNIT_TYPEID::ZERG_EGG)
-			{
-				if (unit.orders.empty())
-				{
-					// can this even happen?
-					return false;
-				}
-				auto ability = get_production_abilities(type);
-				return unit.orders[0].ability_id == AbilityID(ability);
-			}
-			else if ((unit.unit_type.ToType() != type))
-			{
-				return false;
-			}
 
-			return unit.build_progress < 1.0;
-
-			};
-		return obs->GetUnits(f);
-	}
-
-	inline Units get_ready_units(const ObservationInterface* obs, UNIT_TYPEID type)
-	{
-		auto f = [type](const Unit& unit) { return unit.build_progress == 1.0 && IsUnit{ type }(unit); };
-		return obs->GetUnits(f);
-	}
-
-
-	inline const Unit* get_closest_unit(const ObservationInterface* obs, const Point2D pos, const Units& units)
+	inline const Unit* get_closest_unit(const Point2D& pos, const Units& units)
 	{
 		if (units.empty())
 		{
@@ -60,7 +31,7 @@ namespace sc2 {
 		}
 
 		const Unit* closest = units.front();
-		float distance = 1000000;
+		float distance = DistanceSquared2D(closest->pos, pos);
 
 		// start at one intentionally
 		for (int i = 1; i < units.size(); i++)
@@ -79,7 +50,7 @@ namespace sc2 {
 
 	inline const Unit* get_closest_unit(const ObservationInterface* obs, const Point2D pos, UNIT_TYPEID type)
 	{
-		Units units = obs->GetUnits(IsUnit{ type });
+		Units units = obs->GetUnits(sc2::IsUnit{ type });
 		float distance = 0;
 		const Unit* target = nullptr;
 		for (const auto& u : units) {
@@ -97,114 +68,4 @@ namespace sc2 {
 
 		return target;
 	}
-
-	inline bool have_enough_supply(const ObservationInterface* obs, int requested_supply)
-	{
-		auto supply_cap = (int)obs->GetFoodCap();
-		auto supply_used = (int)obs->GetFoodUsed();
-
-		// some units have negative supply cost
-		// because they provide supply (overlord)
-		if (requested_supply <= 0)
-		{
-			return true;
-		}
-
-		// supply block (eg overlord killed)
-		if (supply_used > supply_cap)
-		{
-			return false;
-		}
-
-		auto free_supply = supply_cap - supply_used;
-
-		if (free_supply < requested_supply)
-		{
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-	}
-
-
-	inline bool can_afford(const ObservationInterface* obs, UNIT_TYPEID type)
-	{
-		const auto& unit_data = obs->GetUnitTypeData();
-		auto idx = static_cast<size_t>(type);
-		auto mineral_cost = unit_data[idx].mineral_cost;
-		auto gas_cost = unit_data[idx].vespene_cost;
-		int supply_cost = std::ceil(unit_data[idx].food_required);
-
-		if (obs->GetMinerals() >= mineral_cost
-			&& obs->GetVespene() >= gas_cost
-			&& (have_enough_supply(obs, supply_cost)))
-		{
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-	inline bool train(const ObservationInterface* obs, sc2::ActionInterface* actions, sc2::QueryInterface* queries, UNIT_TYPEID type, int amount = 1, bool allow_queueing = true)
-	{
-		using namespace std;
-		auto p_units = get_production_unit(type);
-
-		if (p_units.empty() || p_units[0] == UNIT_TYPEID::INVALID)
-		{
-			std::cout << "WARNING: Requested builders for " << UnitTypeToName(UnitTypeID(type)) << "(" << UnitTypeID(type) << ")" << " and received none!" << std::endl;
-			return false;
-		}
-
-		auto builder_filter = [&p_units](const Unit& unit) -> bool {return unit.alliance == Unit::Alliance::Self && unit.build_progress == 1.0 && IsUnits{ p_units }(unit);};
-		auto builders = obs->GetUnits(builder_filter);
-
-		const auto& unit_data = obs->GetUnitTypeData();
-
-		auto left_to_train = amount;
-
-		auto pending_units = get_pending_units(obs, type);
-
-		if (pending_units.size() >= builders.size())
-		{
-			return false;
-		}
-
-		int builders_tried = 0;
-
-		while (left_to_train > 0 && have_enough_supply(obs, unit_data[(uint32_t)type].food_required))
-		{
-			auto builder = GetRandomEntry(builders);
-
-			if (builders_tried >= builders.size())
-			{
-				break;
-			}
-
-			// TODO: Allow queuing of stuff
-			if (builder->orders.empty())
-			{
-				// TODO: Use query interface to ask for abilities of unit
-
-				auto abilities = queries->GetAbilitiesForUnit(builder, true, true);
-
-				auto t = builder->unit_type.ToType();
-				auto ability = get_production_abilities(type);
-				assert(ability != ABILITY_ID::INVALID);
-				std::cout << "INFO: Sending command " << AbilityTypeToName(AbilityID(ability)) << "; " << AbilityID(ability) << " to unit " << UnitTypeToName(builder->unit_type) << "; " << UnitTypeID(builder->unit_type) << std::endl;
-				actions->UnitCommand(builder, ability);
-				left_to_train = std::max(left_to_train - 1, 0);
-			}
-			else
-			{
-				builders_tried += 1;
-			}
-		}
-
-		return left_to_train == 0;
-	}
-
 }
